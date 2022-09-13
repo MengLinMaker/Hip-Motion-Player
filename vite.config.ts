bdda15ab8ed2f82727843fd1169ae4bed63d2d01
MengLinMaker <lm8100mystery@gmail.com>
2022-09-14 00:48:02 +1000

    Chnaged vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ let faviconURL = '/favicon.svg'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   root: 'src',
+  build: {
+    outDir: '../dist'
+  },
   plugins: [
     VitePWA({
       includeAssets: [faviconURL],
