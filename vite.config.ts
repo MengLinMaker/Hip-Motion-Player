33d1ec39d7fb45cd5094c616208234c5c1bc189b
MengLinMaker <lm8100mystery@gmail.com>
2022-09-13 17:24:28 +1000

    Configure PWA functionality

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+let faviconURL = '/favicon.svg'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: [faviconURL],
+      manifest: {
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: faviconURL,
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          },
+          {
+            src: faviconURL,
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+    })
+  ]
+})
